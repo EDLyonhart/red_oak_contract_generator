fbf49df9ee06a83988c6c6a9f2c506c77fbb81b9
EDLyonhart <EDLyonhart@gmail.com>
2017-05-10 15:04:11 -0700

    change 'int' to 'integer'

--- db/migrate/20170510215553_create_contracts.rb
+++ db/migrate/20170510215553_create_contracts.rb
@@ -1,9 +1,9 @@
 class CreateContracts < ActiveRecord::Migration[5.0]
   def change
     create_table :contracts do |t|
-      t.int :author
-      t.int :agent
-      t.int :client
+      t.integer :author
+      t.integer :agent
+      t.integer :client
       t.datetime :creation_date
       t.datetime :last_modification
       t.text :table_of_contents
